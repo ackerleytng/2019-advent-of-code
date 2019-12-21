33d2d3c72402a23e7c40ff49b67fbb0528ae4d1a
Ackerley Tng <ackerleytng@gmail.com>
2019-12-21 22:20:32 +0800

    Add day 20 part 2 code

--- 20-donut-maze/core.clj
+++ 20-donut-maze/core.clj
@@ -1,5 +1,6 @@
 (ns donut-maze.core
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string]
+            [clojure.set :as s]))
 
 (defn read-map [s]
   (->> (string/split s #"\n")
@@ -65,11 +66,6 @@
   (and (= \. (whats-at m coordinates))
        (some portal-chars (map (partial whats-at m) (neighbors coordinates)))))
 
-(defn num-portals [m]
-  (/ (->> (m->seq m)
-          (filter #(portal-chars (nth % 2)))
-          count) 2))
-
 (defn all-labeled-coordinates [m]
   (->> (m->seq m)
        (map (fn [[x y c]] [x y]))
@@ -129,9 +125,8 @@
   (let [new-dist (+ current-distance delta)]
     (update distances dst (fn [d] (if d (min d new-dist) new-dist)))))
 
-(defn shortest-distance-from-AA-to-ZZ [m]
-  (let [g (graph m)
-        nodes (set (keys g))
+(defn shortest-distance-from-AA-to-ZZ [g]
+  (let [nodes (set (keys g))
         start (first (filter #(= #{\A} (first %)) nodes))
         end (first (filter #(= #{\Z} (first %)) nodes))]
     (loop [curr start
@@ -145,10 +140,13 @@
                             edges)
             next-unvisited (disj unvisited curr)]
         (if (seq next-unvisited)
-          (let [next-curr (->> (filter #(next-unvisited (first %)) next-distances)
-                               (apply min-key second)
-                               first)]
-            (recur next-curr next-unvisited next-distances))
+          (if-let [next-curr (some->> next-distances
+                                      (filter #(next-unvisited (first %)))
+                                      seq
+                                      (apply min-key second)
+                                      first)]
+            (recur next-curr next-unvisited next-distances)
+            (distances end))
           (distances end))))))
 
 (def test-map-0 "
@@ -215,6 +213,152 @@ YN......#               VT..#....QG
 
 ;; Part 1
 
-(shortest-distance-from-AA-to-ZZ (read-map (slurp "./20-donut-maze/input.txt")))
+(shortest-distance-from-AA-to-ZZ (graph (read-map (slurp "./20-donut-maze/input.txt"))))
 
 ;; => 618
+
+(def test-map-2 "
+             Z L X W       C
+             Z P Q B       K
+  ###########.#.#.#.#######.###############
+  #...#.......#.#.......#.#.......#.#.#...#
+  ###.#.#.#.#.#.#.#.###.#.#.#######.#.#.###
+  #.#...#.#.#...#.#.#...#...#...#.#.......#
+  #.###.#######.###.###.#.###.###.#.#######
+  #...#.......#.#...#...#.............#...#
+  #.#########.#######.#.#######.#######.###
+  #...#.#    F       R I       Z    #.#.#.#
+  #.###.#    D       E C       H    #.#.#.#
+  #.#...#                           #...#.#
+  #.###.#                           #.###.#
+  #.#....OA                       WB..#.#..ZH
+  #.###.#                           #.#.#.#
+CJ......#                           #.....#
+  #######                           #######
+  #.#....CK                         #......IC
+  #.###.#                           #.###.#
+  #.....#                           #...#.#
+  ###.###                           #.#.#.#
+XF....#.#                         RF..#.#.#
+  #####.#                           #######
+  #......CJ                       NM..#...#
+  ###.#.#                           #.###.#
+RE....#.#                           #......RF
+  ###.###        X   X       L      #.#.#.#
+  #.....#        F   Q       P      #.#.#.#
+  ###.###########.###.#######.#########.###
+  #.....#...#.....#.......#...#.....#.#...#
+  #####.#.###.#######.#######.###.###.#.#.#
+  #.......#.......#.#.#.#.#...#...#...#.#.#
+  #####.###.#####.#.#.#.#.###.###.#.###.###
+  #.......#.....#.#...#...............#...#
+  #############.#.#.###.###################
+               A O F   N
+               A A D   M
+")
+
+;; Approach
+;; Find the length of the path from AA to ZZ (without recursion)
+;; The depth of the recursion can't be greater than
+;;   half the length of that path, because we have to
+;;   recurse down and up again. If it were longer, then the direct path
+;;   would be shorter anyway, and we want the shortest path.
+;; So, we can generate all the nodes for all the levels,
+;;   then use dijkstra's to find the shortest path
+
+(defn on-outer-edge?
+  "Determines if this set of coordinates is on the outer edge.
+   Technically doesn't work for all cases - for example, if the
+   map is such that the right edge is only 1 unit thick.
+   For the maps that I'll be operating on, this function will work."
+  [m [x y]]
+  (let [x-max (->> (m->seq m)
+                   (apply max-key (fn [[x y _]] x))
+                   first)
+        y-max (->> (m->seq m)
+                   (apply max-key (fn [[x y _]] y))
+                   second)]
+    (or (> x (- x-max 4)) (< x 4)
+        (> y (- y-max 4)) (< y 4))))
+
+(defn graph [m]
+  (let [labeled-coordinates (->> (all-labeled-coordinates m)
+                                 (map second))
+        labels (map (partial coordinate-label m) labeled-coordinates)
+        portals-grouped (->> (group-by first labels)
+                             (filter (fn [[k v]] (= 2 (count v)))))
+        warp-edges (->> portals-grouped
+                        vals
+                        (mapcat (fn [[a b]] [[a b 1] [b a 1]])))
+        edges (->> (mapcat (partial map-from m) labeled-coordinates)
+                   (remove #(zero? (nth % 2))))]
+    (->> (concat warp-edges edges)
+         (reduce (fn [m [src dst distance]] (assoc-in m [src dst] distance)) {}))))
+
+(defn add-level-to-edge
+  [level [src dst dist]]
+  [(update src 1 conj level) (update dst 1 conj level) dist])
+
+(defn warp-edge
+  [m outer-level portals]
+  (let [[[outer] [inner]] ((juxt filter remove) #(on-outer-edge? m (second %)) portals)
+        inner (update inner 1 conj outer-level)
+        outer (update outer 1 conj (inc outer-level))]
+    [;; Warp from portal on inner edge to a deeper level
+     [inner outer 1]
+     ;; Warp from portal on outer edge back to a portal on the inner edge on outer level
+     [outer inner 1]]))
+
+(defn graph-deep
+  [m num-levels]
+  (let [labeled-coordinates (->> (all-labeled-coordinates m)
+                                 (map second))
+        labels (map (partial coordinate-label m) labeled-coordinates)
+        start (first (filter #(= #{\A} (first %)) labels))
+        end (first (filter #(= #{\Z} (first %)) labels))
+        portals-grouped (->> (group-by first labels)
+                             (filter (fn [[k v]] (= 2 (count v)))))
+        portals (->> portals-grouped
+                     vals
+                     (apply concat))
+        [outer-portals inner-portals]
+        ((juxt filter remove) #(on-outer-edge? m (second %)) portals)
+
+        ;; 0th level: only edges among AA and ZZ, and all inner-portals
+        level-0-portals (conj (set inner-portals) start end)
+        level-0-edges (->> (mapcat (partial map-from m) labeled-coordinates)
+                           (remove #(zero? (nth % 2)))
+                           (filter (fn [[src dst _]] (every? level-0-portals [src dst])))
+                           (map (partial add-level-to-edge 0)))
+
+        within-level-edges (->> (map second portals)
+                                (mapcat (partial map-from m))
+                                (remove #(zero? (nth % 2)))
+                                ;; Remove edges going to AA and ZZ
+                                (remove (fn [[src dst _]] (some #{start end} [src dst]))))
+        all-within-level-edges
+        (mapcat #(map (partial add-level-to-edge %) within-level-edges) (range 1 (inc num-levels)))
+        all-warp-edges
+        (mapcat #(mapcat (fn [[_ ps]] (warp-edge m % ps)) portals-grouped) (range num-levels))]
+    (->> (concat level-0-edges all-warp-edges all-within-level-edges)
+         (reduce (fn [m [src dst distance]] (assoc-in m [src dst] distance)) {}))))
+
+;; Part 2
+
+(let [m (read-map test-map-2)
+      no-recursion-min-distance (shortest-distance-from-AA-to-ZZ (graph m))
+      num-levels (inc (quot no-recursion-min-distance 2))]
+  (shortest-distance-from-AA-to-ZZ (graph-deep m num-levels)))
+
+(comment
+
+  ;; Takes pretty long on the actual one
+
+  (let [m (read-map (slurp "./20-donut-maze/input.txt"))
+        no-recursion-min-distance (shortest-distance-from-AA-to-ZZ (graph m))
+        num-levels (inc (quot no-recursion-min-distance 2))]
+    (shortest-distance-from-AA-to-ZZ (graph-deep m num-levels)))
+
+  )
+
+;; => 7152
